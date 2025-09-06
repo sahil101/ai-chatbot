93e465b830d4e1795c4113ffccb28ff4139b6c7b
Sahil.Agarwal <sahil.agarwal@SahilAgarwals-MacBook-Pro.local>
2025-09-06 14:01:08 +0530

    test husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,9 +3,9 @@ import { Button } from './components/ui/button';
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <Button className="p-3 rounded-2xl">Click Me</Button>
-    </div>
+            <h1>Hello World</h1>
+              <Button className="p-3 rounded-2xl">Click Me</Button>
+      </div>
   );
 }
 
